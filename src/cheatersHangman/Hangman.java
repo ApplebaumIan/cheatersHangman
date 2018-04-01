a164327f78b0c83787036dcba6787c602dfbacd9
Ian Applebaum <ianapplebaum@Ians-iMac.local>
2018-03-31 21:28:54 -0400

    initial commit

---
+++ src/cheatersHangman/Hangman.java
@@ -0,0 +1,13 @@
+package cheatersHangman;
+/***********************
+Jonathan and Ian's Cheaters hangman main code
+***********************/
+public class Hangman {
+	//MAIN METHOD
+	public static void main(String[] args) {
+		System.out.println("Hello World and Jon");
+		// TODO Auto-generated method stub
+		//YEET
+	}
+
+}
